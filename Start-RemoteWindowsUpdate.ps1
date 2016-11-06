65052662db4e68b2ef3856551ebab9beba103573
Ferry <Ferry@blankf.nl>
2016-11-06 10:36:04 +0100

    added the Start-remotewindowsupdate file to the repo

---
+++ Start-RemoteWindowsUpdate.ps1
@@ -0,0 +1,42 @@
+﻿Function Start-Windowsupdate {
+param(
+[Parameter(mandatory=$true)]
+[string]$ServerName,
+
+[Parameter(mandatory=$false)]
+[switch]$InstallRequired,
+
+[Parameter(mandatory=$false)]
+[switch]$ForceReboot
+)
+
+
+#Search Updates
+$sess = New-CimInstance -Namespace root/Microsoft/Windows/WindowsUpdate -ClassName MSFT_WUOperationsSession -CimSession $ServerName
+$scanResults = Invoke-CimMethod -InputObject $sess -MethodName ScanForUpdates -Arguments @{SearchCriteria="IsInstalled=0";OnlineScan=$true}
+
+#display available Updates
+$scanResults.Updates | Select Title,KBArticleID
+
+
+#Install Updates
+If ($InstallRequired)
+{
+ If (($scanResults.Updates).count -gt 0)
+ {
+ 
+ Write-Output "Installing Updates"
+ $scanResults = Invoke-CimMethod -InputObject $sess -MethodName ApplyApplicableUpdates
+
+If ($ForceReboot)
+ {
+ Write-Output "Restarting Node $ServerName"
+ Invoke-Command -ComputerName $ServerName -ScriptBlock {Restart-Computer -Force}
+ }
+ }
+ Else
+ {
+ Write-Warning "No applicaple Updates found"
+  }
+ }
+}
