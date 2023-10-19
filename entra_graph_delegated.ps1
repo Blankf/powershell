fa535ce76193c85f672642671de79778b833ea2e
Ferry Blankendaal <ferry.blankendaal@gmail.com>
2023-10-19 16:07:42 +0200

    Create entra_graph_delegated.ps1

---
+++ entra_graph_delegated.ps1
@@ -0,0 +1,28 @@
+Import-Module Microsoft.Graph.Identity.SignIns
+Import-Module Microsoft.Graph.Applications
+import-module Microsoft.Graph.Users
+
+$clientAppId = ""
+$permissions = @("offline_access", "User.Read", "Files.ReadWrite")
+$scopeToGrant = $permissions -join " "
+
+Connect-MgGraph -Scopes "Application.ReadWrite.All", "User.Read.All", "AppRoleAssignment.ReadWrite.All", "DelegatedPermissionGrant.ReadWrite.All"
+
+# Step 1. Check if a service principal exists for the client application. 
+$clientSp = Get-MgServicePrincipal -Filter "appId eq '$($clientAppId)'"
+
+# Step 2. Get the information for the GraphAPI
+$GraphApi = Get-MgServicePrincipal -Filter "displayName eq 'Microsoft Graph'"
+
+# Step 3. Create a delegated permission that grants the client app access to the
+#     API, on behalf of the user. (This example assumes that an existing delegated 
+#     permission grant does not already exist, in which case it would be necessary 
+#     to update the existing grant, rather than create a new one.)
+$params = @{
+  "ClientId"    = $clientsp.id
+  "ConsentType" = "AllPrincipals"
+  "ResourceId"  = $GraphApi.id
+  "Scope"       = $scopeToGrant
+}
+New-MgOauth2PermissionGrant -BodyParameter $params | 
+Format-List Id, ClientId, ConsentType, ExpiryTime, PrincipalId, ResourceId, Scope
