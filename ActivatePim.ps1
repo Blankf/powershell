0c1b26c37522156608b6e8cb1fd3c080778cf7d7
Ferry Blankendaal <ferry.blankendaal@gmail.com>
2023-10-19 15:39:34 +0200

    Create ActivatePim.ps1

---
+++ ActivatePim.ps1
@@ -0,0 +1,44 @@
+Function Activate-Pim {
+  Param(
+    [int]$Time = 4
+  )
+
+  Import-Module Microsoft.Graph.Authentication
+  Import-Module Microsoft.Graph.Identity.Governance
+
+  Connect-MgGraph -NoWelcome
+  $context = Get-MgContext
+  $currentUser = (Get-MgUser -UserId $context.Account).Id
+  $myRoles = Get-MgRoleManagementDirectoryRoleEligibilitySchedule -ExpandProperty RoleDefinition -All -Filter "principalId eq '$currentuser'"
+
+  Write-Host "Choose one of the following roles:"
+  $results = @()
+  for ($i = 0; $i -lt $myroles.Count; $i++) {
+    Write-Host "$($i + 1): $($($myroles.RoleDefinition.DisplayName)[$i])"
+    $obj = New-Object System.Object
+    $obj | Add-Member -Type NoteProperty -Name Number -Value $($i + 1)
+    $obj | Add-Member -Type NoteProperty -Name Role -Value $($($myroles.RoleDefinition.DisplayName)[$i])
+    $obj | Add-Member -Type NoteProperty -Name RoleId -Value $($($myroles.RoleDefinition.id)[$i])
+    $obj | Add-Member -Type NoteProperty -Name DirId -Value $($($myroles.DirectoryScopeId)[$i])
+    $results += $obj
+  }
+  $choice = Read-Host "Enter the number of the object you want to choose"
+  $myRole = ($results | Where-Object { $_.Number -eq $choice })
+
+  $params = @{
+    Action           = "selfActivate"
+    PrincipalId      = $currentUser
+    RoleDefinitionId = $myRole.RoleId
+    DirectoryScopeId = $myRole.DirId
+    Justification    = "Enable $($myRole.Role) role"
+    ScheduleInfo     = @{
+      StartDateTime = Get-Date
+      Expiration    = @{
+        Type     = "AfterDuration"
+        Duration = "PT$($Time)H"
+      }
+    }
+  }
+
+  New-MgRoleManagementDirectoryRoleAssignmentScheduleRequest -BodyParameter $params
+}
