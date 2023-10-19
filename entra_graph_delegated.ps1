b81020b79e97397f17b1401b112e28c36967d0c0
Ferry Blankendaal <ferry.blankendaal@gmail.com>
2023-10-19 16:35:58 +0200

    Update entra_graph_delegated.ps1

--- entra_graph_delegated.ps1
+++ entra_graph_delegated.ps1
@@ -1,15 +1,20 @@
+# ---- Creating a new delegated permission ---- # 
 Import-Module Microsoft.Graph.Identity.SignIns
 Import-Module Microsoft.Graph.Applications
 import-module Microsoft.Graph.Users
 
 $clientAppId = ""
+$clientAppName = ""
 $permissions = @("offline_access", "User.Read", "Files.ReadWrite")
 $scopeToGrant = $permissions -join " "
 
 Connect-MgGraph -Scopes "Application.ReadWrite.All", "User.Read.All", "AppRoleAssignment.ReadWrite.All", "DelegatedPermissionGrant.ReadWrite.All"
 
 # Step 1. Check if a service principal exists for the client application. 
+$clientsp = Get-MgServicePrincipal -Filter "displayName eq '$($clientAppName)'"
+or
 $clientSp = Get-MgServicePrincipal -Filter "appId eq '$($clientAppId)'"
+
 
 # Step 2. Get the information for the GraphAPI
 $GraphApi = Get-MgServicePrincipal -Filter "displayName eq 'Microsoft Graph'"
@@ -26,3 +31,40 @@ $params = @{
 }
 New-MgOauth2PermissionGrant -BodyParameter $params | 
 Format-List Id, ClientId, ConsentType, ExpiryTime, PrincipalId, ResourceId, Scope
+
+# ----- Adding a delegated permission ---- #
+
+$clientAppName = ""
+$permissions = @("User.Read.All")
+$PermissionsToAdd = $permissions -join " "
+
+# Step 1. Check if a service principal exists for the client application. 
+$clientsp = Get-MgServicePrincipal -Filter "displayName eq '$($clientAppName)'"
+
+# Step 2. select the delegation that is for "AllPrincipals"
+$updgrant = Get-MgOauth2PermissionGrant -Filter "clientId eq '$($clientsp.id)' and consentType eq 'AllPrincipals'"
+
+# Step 3. Add the permissions
+$newscope = $($updgrant.Scope) + " " + $PermissionsToAdd
+
+# Step 4. Apply the new scopes.
+Update-MgOauth2PermissionGrant -OAuth2PermissionGrantId $updgrant.Id -Scope $newscope
+
+# ----- Remove a delegated permission ---- #
+
+$clientAppName = ""
+$permissionsToRemove = @("User.Read.All","Group.Read.All")
+
+# Step 1. Check if a service principal exists for the client application. 
+$clientsp = Get-MgServicePrincipal -Filter "displayName eq '$($clientAppName)'"
+
+# Step 2. select the delegation that is for "AllPrincipals"
+$updgrant = Get-MgOauth2PermissionGrant -Filter "clientId eq '$($clientsp.id)' and consentType eq 'AllPrincipals'"
+
+# Step 3. Add the permissions
+$temp = $updgrant.Scope -split " "
+$filteredPermissions = $temp | Where-Object { $_ -notin $permissionsToRemove }
+$newscope = $filteredPermissions -join " "
+
+# Step 4. Apply the new scopes.
+Update-MgOauth2PermissionGrant -OAuth2PermissionGrantId $updgrant.Id -Scope $newscope
