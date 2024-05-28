e05847a1f467a7b228364a8e7ce3b2a4d2935be5
Ferry Blankendaal <ferry.blankendaal@gmail.com>
2024-05-28 14:40:48 +0200

    Create invoice_functions.ps1

---
+++ halopsa/invoice_functions.ps1
@@ -0,0 +1,45 @@
+Function Update-HaloRecuringInvoice {
+  <#
+  .SYNOPSIS
+  Updates the recurring invoice for a customer in the Halo system.
+
+  .DESCRIPTION
+  This function updates the recurring invoice for a customer in the Halo system. It retrieves the client ID based on the provided customer name or the specified client ID. If the client ID is not provided, it will be retrieved based on the customer name.
+
+  .PARAMETER AzureCosts
+  The new value for the Azure costs.
+
+  .PARAMETER Customer
+  The name of the customer. Either the customer name or the client ID must be specified.
+
+  .PARAMETER ClientID
+  The ID of the client. Either the customer name or the client ID must be specified.
+
+  .EXAMPLE
+  Update-HaloRecuringInvoice -AzureCosts 1000 -Customer "Contoso"
+
+  .EXAMPLE
+  Update-HaloRecuringInvoice -AzureCosts 1000 -ClientID "12345"
+  #>
+
+  param (
+    [Parameter(Mandatory=$true)]
+    $AzureCosts,
+    [Parameter(Mandatory = $true, ParameterSetName = "Customer")]
+    [String]$Customer,
+    [Parameter(Mandatory = $true, ParameterSetName = "ClientId")]
+    [String]$ClientID
+  )
+
+  $AzureCosts = [decimal]::Parse($AzureCosts, [System.Globalization.CultureInfo]::GetCultureInfo("nl-NL"))
+
+  if (!($ClientID)) {
+    $HaloClients = Get-HaloClient
+    $ClientId = $HaloClients | where-object { $_.Name -eq $Customer } | Select-Object -ExpandProperty Id
+  }
+
+  $Invoice = Get-HaloRecurringInvoice -ClientID $ClientId -includeLines
+  ($invoice.lines | Where-Object { $_.nominal_code -eq 'AZU001' }).unit_price = $AzureCosts
+  $output = Set-HaloRecurringInvoice -RecurringInvoice $Invoice
+
+}
